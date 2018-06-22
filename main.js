3bb838ee39e360b2501079651f34323c9b21deda
Edward Rutz <9n7o7xnlyabt@blurmail.net>
2018-06-22 10:57:19 -0400

    Completed multi word text search

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 //Refactor for ECMAScript 5.1
+console.log('main.js loaded');
 
+//Input keywords and convert to array
+let keywordsIn = document.getElementById("keywordsIn").value;
+console.log('keywordsIn', keywordsIn);
+let keywords = keywordsIn.split(',');
+console.log('keywords', keywords);
 
-// let keywords = reportInput.split(',');
-// let reporting = reportInput;
-var reporting = "blue green blue yellow";
-var keywords = ["blue", "green", "yellow"];
+//Input reporting
+let reporting = document.getElementById("reportingIn").value;
+// var reporting = "blue green blue green yellow blue red purple";
+console.log('reporting', reporting);
+// var keywords = ["red", "blue", "yellow"];
 var match ='';
 
 function textSearch() {
@@ -28,12 +35,26 @@ function textSearch() {
 
   // return matchObj;
 
-//Output Keywords
-  let res = keywords[0] + ' = ' + matchObj[keywords[0]].count;
-  console.log('count', res);
+  //Output Keywords
+  var res='';
+  var i;
+  for (i = 0; i < keywords.length; i++) {
+    console.log('i', i);
+    console.log('keywords.length', keywords.length);
+    console.log('keyword[i]', keywords[i]);
+    console.log('matchObj[keywords[i]].count', matchObj[keywords[i]].count);
+     res += keywords[i] + ' = ' + matchObj[keywords[i]].count + '<br>';
+    }
+    console.log('keyword & count', res);
   document.getElementById("results").innerHTML = res;
 }
 
-
-
-
+function resetForm() {
+  document.getElementById("keywordsIn").value = "";
+  document.getElementById("reportingIn").value = "";
+  // document.getElementById("results").value = "";
+  // document.getElementById('keywordsIn').reset();
+  // document.getElementById('reportingIn').reset();
+  // element.value = '';
+  // isSubmitted = false;
+}
