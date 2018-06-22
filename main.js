0cc43dc0bd81c754475521987d9ec82ab0e8e4c7
Edward Rutz <9n7o7xnlyabt@blurmail.net>
2018-06-22 12:55:11 -0400

    Added keyword search

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 //Refactor for ECMAScript 5.1
 console.log('main.js loaded');
 
-//Input keywords and convert to array
 let keywordsIn = document.getElementById("keywordsIn").value;
-console.log('keywordsIn', keywordsIn);
 let keywords = keywordsIn.split(',');
-console.log('keywords', keywords);
-
-//Input reporting
 let reporting = document.getElementById("reportingIn").value;
-// var reporting = "blue green blue green yellow blue red purple";
-console.log('reporting', reporting);
-// var keywords = ["red", "blue", "yellow"];
+  console.log('reporting', reporting);
 var match ='';
+
+function mainEvent() {
+  textSearch();
+}
 
 function textSearch() {
   var matchObj = {};
@@ -39,13 +36,8 @@ function textSearch() {
   var res='';
   var i;
   for (i = 0; i < keywords.length; i++) {
-    console.log('i', i);
-    console.log('keywords.length', keywords.length);
-    console.log('keyword[i]', keywords[i]);
-    console.log('matchObj[keywords[i]].count', matchObj[keywords[i]].count);
      res += keywords[i] + ' = ' + matchObj[keywords[i]].count + '<br>';
     }
-    console.log('keyword & count', res);
   document.getElementById("results").innerHTML = res;
 }
 
@@ -57,4 +49,8 @@ function resetForm() {
   // document.getElementById('reportingIn').reset();
   // element.value = '';
   // isSubmitted = false;
+}
+
+function numberSearch {
+  
 }
