630b5be25fec981feb94104e4d166c5e67ae7c04
Edward Rutz <9n7o7xnlyabt@blurmail.net>
2018-06-25 12:50:44 -0400

    Added instructions to index.html
    Added css and comments to textSearch.

--- main.js
+++ main.js
@@ -7,15 +7,25 @@ let reporting = document.getElementById("reportingIn").value;
   console.log('reporting', reporting);
 var match ='';
 
+console.log('keywords', keywords);
+console.log('reporting', reporting);
+
 function mainEvent() {
   textSearch();
 }
 
 function textSearch() {
+  console.log('Running textSearch...');
+
   var matchObj = {};
   keywords.forEach(word => {
     let pattern = new RegExp(word, 'ig');
+    console.log('pattern', pattern);
+
     while ((match = pattern.exec(reporting))) {
+        console.log('!matchObj[match[0]] term match returns false...', !matchObj[match[0]]);
+
+      //If matchObj[match[0]] is false
       if (!matchObj[match[0]]) {
         matchObj[match[0]] = {};
         matchObj[match[0]].match = match[0];
@@ -23,9 +33,14 @@ function textSearch() {
         matchObj[match[0]].indexes.add(match.index);
         matchObj[match[0]].hitLength = match[0].length;
         matchObj[match[0]].count = matchObj[match[0]].indexes.size;
+        console.log('matchObj[match[0]].count', matchObj[match[0]].count);
+        console.log('matchObj[match[0]]', matchObj[match[0]]);
+
       } else {
         matchObj[match[0]].indexes.add(match.index);
         matchObj[match[0]].count = matchObj[match[0]].indexes.size;
+        console.log('add term location to set...', matchObj[match[0]].indexes.add(match.index));
+        console.log('add term count to set...', matchObj[match[0]].count);
       }
     }
   });
@@ -51,6 +66,6 @@ function resetForm() {
   // isSubmitted = false;
 }
 
-function numberSearch {
-  
+function numberSearch() {
+
 }
